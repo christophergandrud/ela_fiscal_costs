fec71b555ed5c17d8728e00385b48695a296197e
christophergandrud <christopher.gandrud@gmail.com>
2016-08-10 10:54:18 -0400

    liquid formula

--- formal_modelling/ela_balance_sheet_effects.py
+++ formal_modelling/ela_balance_sheet_effects.py
@@ -9,12 +9,19 @@ import sympy as sp
 from sympy.abc import eta, gamma
 
 # Define symbols (eta and gamma imported)
-A, D = sp.symbols('A A_d')
+A, AN, AP, la,  AD = sp.symbols('A A_N, A_P, \lambda A_d')
 
 #######
-# Expanded form expression for non-performing loans
-expr_npl1 = (gamma * A) / A
-expr_npl2 = (gamma * A - eta * gamma * D)/ (A - D)
+# Simplified form of proportion of liquid assets
+lambdaA = (1 - la) * gamma * AN + la * (1 - gamma) * AP
 
-# Subtract and print to LaTeX`
+print(sp.latex(lambdaA))
+
+#######
+# Expanded form expression for the change in non-performing loans after ELA
+# collateral seizure
+expr_npl1 = (gamma * A) / A
+expr_npl2 = (gamma * A - eta * gamma * AD)/ (A - AD)
+
+# Subtract and print simplified form as LaTeX
 print(sp.latex((expr_npl1 - expr_npl2)))
